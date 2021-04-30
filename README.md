bc7cc85bc151d7dd908900288f7029c35178b0dc
christianmarquesos@poli.ufrj.br <christianmarquesos@poli.ufrj.br>
2021-04-30 12:26:03 -0300

    Organização inicial dos arquivos

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# T1-Sistema_apoio_decisao_aprovacao_credito
+# Sistema de apoio à aprovação de Crédito
 Trabalho 1 de Introdução ao Aprendizado de Máquina. Tem por objetivo a criação de um classificador para apoio à decisão de aprovação de crédito.
+
+## 1. Pré-processamento
+## 2. Seleção de atributos
+## 3. Implementação
+### 3.1. Escolha do modelo preditivo
+### 3.2. Script
+### 3.3. Ajustes e hiperparâmetros
+## 4. Análise geral do desempenho
---
+++ main.py
@@ -0,0 +1,9 @@
+#################################################################################
+# Universidade Federal do Rio de Janeiro
+# Disciplina: Introdução ao Aprendizado de Máquina - EEL891
+# Professor: Heraldo
+# Desenvolvedor: Chritian Marques de Oliveira Silva
+# DRE: 117.214.742
+# Trabalho 1: Classificação - Sistema de apoio à decisão p/ aprovação de crédito
+#################################################################################
+
